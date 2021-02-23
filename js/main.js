0e1a408ee9f2f1265c7db63f52467224fc734839
shiro404-kr <78129297+shiro404-kr@users.noreply.github.com>
2021-02-23 09:59:45 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,31 +6,37 @@ $(function(){
 
 
 $(function() {
+    let dutation = 0;
+
+    // all
+    $('#btn-all').on('click', function(){
+      $('#works .list li').show(dutation);
+    })
 
     $('#btn-all').on('click', function() {
         $('#works .list li').show(400);
-    });
+    })
     
     $('#btn-web').on('click', function() {
         $('#works .list li')
         .hide(500)
         .filter('.web')
         .show(400);
-    });
+    })
 
     $('#btn-graphic').on('click', function() {
         $('#works .list li')
         .hide(500)
         .filter('.graphic')
         .show(400);
-    });
+    })
 
     $('#btn-home').on('click', function() {
         $('#works .list li')
         .hide(500)
         .filter('.home')
         .show(400);
-    });
+    })
 
 
 
