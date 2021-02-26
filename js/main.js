6423749eefd6bcc1c695103a472d743b038fdf74
shiro404-kr <78129297+shiro404-kr@users.noreply.github.com>
2021-02-26 17:24:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,39 +6,17 @@ $(function(){
 
 
 $(function() {
-    let dutation = 0;
 
-    // all
-    $('#btn-all').on('click', function(){
-      $('#works .list li').show(dutation);
-    })
-
-    $('#btn-all').on('click', function() {
-        $('#works .list li').show(400);
-    })
-    
-    $('#btn-web').on('click', function() {
-        $('#works .list li')
-        .hide(500)
-        .filter('.web')
-        .show(400);
-    })
-
-    $('#btn-graphic').on('click', function() {
-        $('#works .list li')
-        .hide(500)
-        .filter('.graphic')
-        .show(400);
-    })
-
-    $('#btn-home').on('click', function() {
-        $('#works .list li')
-        .hide(500)
-        .filter('.home')
-        .show(400);
-    })
-
-
+    // gnb 열기
+    $('.gnb_menu').on('click', function(){
+      $('#gnb-mob').addClass('show');
+      console.log('click');
+    });
+  
+    // gnb 닫기
+    $('.gnb_mc').on('click', function(){
+      $('#gnb-mob').removeClass('show');
+    });
 
 })
 
